47d607ac68fab979cc149f27b6e18d7345c254fa
ramana1718 <gokulramana1718@gmail.com>
2024-06-13 11:16:24 +0530

    signIn and signUp page developed

--- server/index.js
+++ server/index.js
@@ -1,1 +1,31 @@
-console.log("hello,world");
+//console.log("hello,world");
+
+const express = require('express');
+const mongoose=require('mongoose');
+const authRouter =require("./routes/auth");
+
+const PORT =3000;
+const app=express();
+const DB="mongodb+srv://gokulramana:030406@cluster0.d203sag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+app.use(express.json());
+app.use(authRouter);
+
+// app.get("/",(req,res)=>{
+//     res.send("GOkul");
+// });
+// app.get("/hello-world",(req, res)=>{
+//     res.json({hi:"hello world"});
+// })
+mongoose.connect(DB).then(()=>{
+    console.log("connection successful")
+}).catch((e)=>{
+    console.log(e);
+})
+
+app.listen(PORT,()=>{
+    console.log(`connected at port ${PORT} `);
+
+});
+
+
